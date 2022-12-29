b75f3bd32d7e76b9a1d0f524df67bfdbaffb4aab
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-12-29 12:56:55 -0800

    agree.c

---
+++ Conditionals/agree.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+    printf("Do you agree? ");
+    scanf("%c", &c);
+
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not agreed.\n");
+    }
+    else{
+        printf("Please input either y or n");
+    }
+}
