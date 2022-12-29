9c289491ad7788b01ceaf702f9d85ff35d9f9e34
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-12-29 11:15:18 -0800

    if else statement

---
+++ Conditionals/if_else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 5;
+    int y = 5;
+
+    if (x > y)
+    {
+        printf("x is greater than y!");
+    }
+    else if (x < y)
+    {
+        printf("x is less than y!");
+    }
+    else if (x = y)
+    {
+        printf("x is equal to y!");
+    }
+}
