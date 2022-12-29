8a02c5f3982eeb9942bfb2d2db5f08495e2ecec4
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-12-29 11:19:31 -0800

    refactor code.

--- Conditionals/if_else.c
+++ Conditionals/if_else.c
@@ -7,14 +7,14 @@ int main(void)
 
     if (x > y)
     {
-        printf("x is greater than y!");
+        printf("x is greater than y!\n");
     }
     else if (x < y)
     {
-        printf("x is less than y!");
+        printf("x is less than y!\n");
     }
-    else if (x == y)
+    else
     {
-        printf("x is equal to y!");
+        printf("x is equal to y!\n");
     }
 }
