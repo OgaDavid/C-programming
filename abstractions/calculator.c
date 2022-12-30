8feec67d1c1bee28a98396bdcb9ee815e7ac0bf0
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-12-30 02:58:56 -0800

    calculator in c

---
+++ abstractions/calculator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// declaring functions
+
+
+int main(void)
+{
+    // get inputs
+    int x, y;
+    char operation;
+
+    printf("What is the operation? Reply with either ( +, -, *, /): ");
+    scanf("%c", &operation);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    if (operation == '+')
+    {
+        printf("%d + %d = %d", x, y, x + y);
+    }
+    else if (operation == '-')
+    {
+        printf("%d - %d = %d", x, y, x - y);
+    }
+    else if (operation == '*')
+    {
+        printf("%d * %d = %d", x, y, x * y);
+    }
+    else if (operation == '/')
+    {
+        printf("%d / %d = %d", x, y, x / y);
+    }
+    else{
+        printf("Invalid operator or variable.");
+    }
+
+    
+
+    return 0;
+
+}
+
+
+// A CALCULATOR THAT PERFORMS BASIC OPERATIONS LIKE + - / * %
+
+// get the inputs
+// write out functions for each operator
+// 
