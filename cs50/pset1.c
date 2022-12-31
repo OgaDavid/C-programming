31bdbefff455877d85b7652c0252d72532d11fd5
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-12-30 23:28:28 -0800

    mario block

--- cs50/pset1.c
+++ cs50/pset1.c
@@ -2,7 +2,38 @@
 
 int main(void)
 {
-    int height;
+    int height, row, col, space;
+
+    do
+    {
+        printf("Height: ");
+        scanf("%d", &height);
+    } 
+    while (height < 1 || height > 8);
+
+    for (row = 0; row < height; row++)
+    {
+        for (space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+
+        for (col = 0; col <= row; col++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (col = 0; col <= row; col++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+    
 }
 
 // prompt user for input
+// implement condition to continuing prompting if user enters num less than one and greater than 8.
